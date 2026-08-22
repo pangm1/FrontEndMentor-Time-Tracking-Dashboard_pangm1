d9fbc3abc4e2ef1f3db215da852c12b1ef6ad404
Michael Pang <pangm1@tcnj.edu>
2026-08-22 00:13:53 -0400

    Change dashboard attribute with javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-console.log(document);
+// console.log(document);
+
+const dashboard = document.querySelector('.dashboard');
+const dailyButton = document.querySelector('.dashboard header nav button.daily');
+const weeklyButton = document.querySelector('.dashboard header nav button.weekly');
+const monthlyButton = document.querySelector('.dashboard header nav button.monthly');
+// console.log(dailyButton);
+// console.log(weeklyButton);
+// console.log(monthlyButton);
+// console.log(dashboard.attributes.timeframe);
+
+dailyButton.addEventListener('click', () => changeDashboard("daily"));
+weeklyButton.addEventListener('click', () => changeDashboard("weekly"));
+monthlyButton.addEventListener('click', () => changeDashboard("monthly"));
+
+function changeDashboard(s) { 
+    // console.log("Clicked " + s);
+    dashboard.setAttribute("timeframe", s);
+}
